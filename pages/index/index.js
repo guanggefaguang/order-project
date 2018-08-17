fb99bc4db134c1caba931c45d8653d38f566abc8
zhongyc03 <zhongyc03@vanke.com>
2018-08-17 15:59:39 +0800

    微信扫码点餐
    微信扫码点餐

---
+++ pages/index/index.js
@@ -0,0 +1,101 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    menus: [{
+      id: 1,
+      menu: '菜单一'
+    }, {
+      id: 1,
+      menu: '菜单一'
+    }, {
+      id: 1,
+      menu: '菜单一'
+    }, {
+      id: 1,
+      menu: '菜单二'
+    }, {
+      id: 1,
+      menu: '菜单三'
+    }, {
+      id: 1,
+      menu: '菜单四'
+    }, {
+      id: 1,
+      menu: '菜单五'
+    }, {
+      id: 1,
+      menu: '菜单五'
+    }, {
+      id: 1,
+      menu: '菜单五'
+    }, {
+      id: 1,
+      menu: '菜单五'
+    }, {
+      id: 1,
+      menu: '菜单五'
+    }, {
+      id: 1,
+      menu: '菜单五'
+    }, {
+      id: 1,
+      menu: '菜单五'
+    }, {
+      id: 1,
+      menu: '菜单五'
+    }, {
+      id: 1,
+      menu: '菜单五'
+    }, {
+      id: 1,
+      menu: '菜单五'
+    }],
+    items: [{
+      id: 1,
+      title: '湖南辣椒小炒肉',
+      price: 15
+    }, {
+      id: 1,
+      title: '湖南辣椒小炒肉',
+      price: 15
+    }, {
+      id: 1,
+      title: '湖南辣椒小炒肉',
+      price: 15
+    }, {
+      id: 1,
+      title: '湖南辣椒小炒肉',
+      price: 15
+    }, {
+      id: 1,
+      title: '湖南辣椒小炒肉',
+      price: 15
+    }, {
+      id: 1,
+      title: '湖南辣椒小炒肉',
+      price: 15
+    }, {
+      id: 1,
+      title: '湖南辣椒小炒肉',
+      price: 15
+    }, {
+      id: 1,
+      title: '湖南辣椒小炒肉',
+      price: 15
+    }, {
+      id: 1,
+      title: '湖南辣椒小炒肉',
+      price: 15
+    }, {
+      id: 1,
+      title: '湖南辣椒小炒肉',
+      price: 15
+    }]
+  },
+  onLoad: function() {
+
+  }
+})
